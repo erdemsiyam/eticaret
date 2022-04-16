28b81b5ba0464fd1b70d42ce393f5efe96cc6d04
Erdem Siyam <erdemsiyam@gmail.com>
2022-04-16 20:27:06 +0300

    api tarafında option düzenlemeleri bitti

--- api/main.py
+++ api/main.py
@@ -1,19 +1,24 @@
 from distutils import core
 from re import U
 from urllib import response
-from fastapi import Depends, FastAPI,HTTPException,status
+from fastapi import Depends, FastAPI,HTTPException,status,File,UploadFile
+from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from fastapi.exceptions import HTTPException
 from pydantic import BaseModel
 from fastapi_jwt_auth import AuthJWT
 import uuid
-from typing import Optional
+from typing import Optional,List
 import os
+
 
 IMAGEDIR = os.path.abspath(os.getcwd()) + '/images/'
 
 app = FastAPI()
+app.mount("/images", StaticFiles(directory="images"), name="static")
+URL = "http://127.0.0.1:8000/images/"
 #uvicorn main:app --reload
+#http://127.0.0.1:8000
 
 # REPOSITORY
 class User(BaseModel):
@@ -37,157 +42,397 @@ user2.email = "siyam@erdem.com"
 user2.password = "123"
 users.append(user2)
 
-class Picture(BaseModel):
-    uuid:Optional[str]
-    name:Optional[str]
-pictures = []
-for i in range(1,31):
-    pic = Picture()
-    pic.uuid = str(i)
-    pic.name = pic.uuid + '.jpg'
-    pictures.append(pic)
+# class Picture(BaseModel):
+#     uuid:Optional[str]
+#     name:Optional[str]
+# pictures = []
+# for i in range(1,31):
+#     pic = Picture()
+#     pic.uuid = str(i)
+#     pic.name = pic.uuid + '.jpg'
+#     pictures.append(pic)
 
 class Category(BaseModel):
     uuid:Optional[str]
-    picture_uuid:Optional[str]
+    image:Optional[str]
     title:Optional[str]
 categories = []
 cat1 = Category()
 cat1.uuid = "1"
-cat1.picture_uuid = "1"
+cat1.image = URL + "1.jpg"
 cat1.title = "Sneakers"
 categories.append(cat1)
 cat2 = Category()
 cat2.uuid = "2"
-cat2.picture_uuid = "2"
+cat2.image = URL + "2.jpg"
 cat2.title = "Jacket"
 categories.append(cat2)
 cat3 = Category()
 cat3.uuid = "3"
-cat3.picture_uuid = "3"
+cat3.image = URL + "3.jpg"
 cat3.title = "Watch"
 categories.append(cat3)
 
 
-class Option(BaseModel):
-    size:Optional[int]
-    color:Optional[str]
+# class Option(BaseModel):
+#     size:Optional[int]
+#     color:Optional[str]
     
+#     @classmethod
+#     def custom_init(cls, size:Optional[int], color:Optional[str]):
+#         return cls(size=size, color=color)
+class Option(BaseModel):
+    type:Optional[str]
+    value:Optional[str]
+    options:Optional[List[object]]
     @classmethod
-    def custom_init(cls, size:Optional[int], color:Optional[str]):
-        return cls(size=size, color=color)
+    def custom_init(cls, new_value:Optional[str], new_options: Optional[List[object]] = []):
+        return cls(value=new_value,options = new_options)
+class OptionTypes:
+    SHOES_NUMBER = "SHOES_NUMBER"
+    BODY_NUMBER = "BODY_NUMBER"
+    WRIST_NUMBER = "WRIST_NUMBER"
+    SIZE = "SIZE"
+    COLOR = "COLOR"
+    MATERIAL = "MATERIAL"
+class ShoesNumberOption(Option):
+    type = OptionTypes.SHOES_NUMBER
+class BodyNumberOption(Option):
+    type = OptionTypes.BODY_NUMBER
+class WristNumberOption(Option):
+    type = OptionTypes.WRIST_NUMBER
+class SizeOption(Option):
+    type = OptionTypes.SIZE
+class ColorOption(Option):
+    type = OptionTypes.COLOR
+class MaterialOption(Option):
+    type = OptionTypes.MATERIAL
+class Sizes:
+    SMALL = "SMALL"
+    MEDIUM = "MEDIUM"
+    LARGE = "LARGE"
+    XLARGE = "XLARGE"
+class Numbers:
+    N10 = "10"
+    N11 = "11"
+    N12 = "12"
+    N36 = "36"
+    N38 = "38"
+    N39 = "39"
+    N40 = "40"
+    N41 = "41"
+    N42 = "42"
+    N44 = "44"
+    N54 = "54"
+    N60 = "60"
+    N64 = "64"
+class Colors:
+    BLUE = "BLUE"
+    RED = "RED"
+    WHITE = "WHITE"
+    ORANGE = "ORANGE"
+    BLACK = "BLACK"
+    PINK = "PINK"
+    YELLOW = "YELLOW"
+class Materials:
+    GOLD = "GOLD"
+    PLATINIUM = "PLATINIUM"
+    SILVER = "SILVER"
+
 
 class Item(BaseModel):
     uuid:Optional[str]
     category_uuid:Optional[str]
-    small_picture_uuid:Optional[str]
-    pictures:Optional[list[str]]
+    small_image:Optional[str]
+    images:Optional[List[str]]
     title:Optional[str]
     description:Optional[str]
     price:Optional[float]
     point:Optional[int]
-    options:Optional[list[Option]]
-    selected_option:Optional[Option] # Sepet için eklenildi, kolaylık olsun diye
+    # options:Optional[list[Option]]
+    # selected_option:Optional[Option] # Sepet için eklenildi, kolaylık olsun diye
+    option_types:Optional[List[str]]
+    options:Optional[List[Option]]
+    selected_option:Optional[Option]
     cart_item_uuid:Optional[str] # Sepete eklenirse, o sepetteki idsi buraya koyulur, sadece sepet için eklenildi bu alan kolaylık olsun diye
-items:list[Item] = []
+items:List[Item] = []
 item1 = Item()
 item1.uuid = "4"
 item1.category_uuid = "1"
-item1.small_picture_uuid = "4"
-item1.pictures = ["5","6"]
+item1.small_image = URL + "4.jpg"
+item1.images = [URL + "5.jpg",URL + "6.jpg"]
 item1.title = "Shoes 1"
 item1.description = "Shoes 1 Description"
 item1.price = 100
 item1.point = 3
-item1.options = [Option.custom_init(40,"blue"),Option.custom_init(40,"red"),Option.custom_init(41,"black")]
+# item1.options = [Option.custom_init(40,"blue"),Option.custom_init(40,"red"),Option.custom_init(41,"black")]
+item1.option_types = [OptionTypes.SHOES_NUMBER,OptionTypes.COLOR]
+item1.options = [
+    ShoesNumberOption.custom_init(
+        new_value=Numbers.N40,
+        new_options=[
+            ColorOption().custom_init(new_value=Colors.BLUE),
+            ColorOption().custom_init(new_value=Colors.RED),
+        ],
+    ),
+    ShoesNumberOption.custom_init(
+        new_value=Numbers.N41,
+        new_options=[
+            ColorOption().custom_init(new_value=Colors.BLACK),
+        ],
+    ),
+]
 items.append(item1)
 item2 = Item()
 item2.uuid = "5"
 item2.category_uuid = "1"
-item2.small_picture_uuid = "7"
-item2.pictures = ["8","9"]
+item2.small_image = URL + "7.jpg"
+item2.images = [URL + "8.jpg",URL + "9.jpg"]
 item2.title = "Shoes 2"
 item2.description = "Shoes 2 Description"
 item2.price = 200
 item2.point = 4
-item2.options = [Option.custom_init(40,"orange"),Option.custom_init(40,"pink"),Option.custom_init(41,"white")]
+# item2.options = [Option.custom_init(40,"orange"),Option.custom_init(40,"pink"),Option.custom_init(41,"white")]
+item2.option_types = [OptionTypes.SHOES_NUMBER,OptionTypes.COLOR]
+item2.options = [
+    ShoesNumberOption.custom_init(
+        new_value=Numbers.N40,
+        new_options=[
+            ColorOption().custom_init(new_value=Colors.ORANGE),
+            ColorOption().custom_init(new_value=Colors.PINK),
+        ],
+    ),
+    ShoesNumberOption.custom_init(
+        new_value=Numbers.N41,
+        new_options=[
+            ColorOption().custom_init(new_value=Colors.WHITE),
+        ],
+    ),
+]
 items.append(item2)
 item3 = Item()
 item3.uuid = "6"
 item3.category_uuid = "1"
-item3.small_picture_uuid = "10"
-item3.pictures = ["11","12"]
+item3.small_image = URL + "10.jpg"
+item3.images = [URL + "11.jpg",URL + "12.jpg"]
 item3.title = "Shoes 3"
 item3.description = "Shoes 3 Description"
 item3.price = 250
 item3.point = 5
-item3.options = [Option.custom_init(39,"orange"),Option.custom_init(41,"yellow"),Option.custom_init(44,"black")]
+# item3.options = [Option.custom_init(39,"orange"),Option.custom_init(41,"yellow"),Option.custom_init(44,"black")]
+item3.option_types = [OptionTypes.SHOES_NUMBER,OptionTypes.COLOR]
+item3.options = [
+    ShoesNumberOption.custom_init(
+        new_value=Numbers.N39,
+        new_options=[
+            ColorOption().custom_init(new_value=Colors.ORANGE),
+        ],
+    ),
+    ShoesNumberOption.custom_init(
+        new_value=Numbers.N41,
+        new_options=[
+            ColorOption().custom_init(new_value=Colors.YELLOW),
+        ],
+    ),
+    ShoesNumberOption.custom_init(
+        new_value=Numbers.N44,
+        new_options=[
+            ColorOption().custom_init(new_value=Colors.BLACK),
+        ],
+    ),
+]
 items.append(item3)
 item4 = Item()
 item4.uuid = "7"
 item4.category_uuid = "2"
-item4.small_picture_uuid = "13"
-item4.pictures = ["14","15"]
+item4.small_image = URL + "13.jpg"
+item4.images = [URL + "14.jpg",URL + "15.jpg"]
 item4.title = "Jacket 1"
 item4.description = "Jacket 1 Description"
 item4.price = 300
 item4.point = 3
-item4.options = [Option.custom_init(60,"Medium"),Option.custom_init(54,"Small"),Option.custom_init(64,"Large")]
+# item4.options = [Option.custom_init(60,"Medium"),Option.custom_init(54,"Small"),Option.custom_init(64,"Large")]
+item4.option_types = [OptionTypes.BODY_NUMBER,OptionTypes.SIZE]
+item4.options = [
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N54,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.SMALL),
+        ],
+    ),
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N60,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.MEDIUM),
+        ],
+    ),
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N64,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.LARGE),
+        ],
+    ),
+]
 items.append(item4)
 item5 = Item()
 item5.uuid = "8"
 item5.category_uuid = "2"
-item5.small_picture_uuid = "16"
-item5.pictures = ["17","18"]
+item5.small_image = URL + "16.jpg"
+item5.images = [URL + "17.jpg",URL + "18.jpg"]
 item5.title = "Jacket 2"
 item5.description = "Jacket 2 Description"
 item5.price = 350
 item5.point = 4
-item5.options = [Option.custom_init(60,"Medium"),Option.custom_init(54,"Small"),Option.custom_init(64,"Large")]
+# item5.options = [Option.custom_init(60,"Medium"),Option.custom_init(54,"Small"),Option.custom_init(64,"Large")]
+item5.option_types = [OptionTypes.BODY_NUMBER,OptionTypes.SIZE]
+item5.options = [
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N54,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.SMALL),
+        ],
+    ),
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N60,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.MEDIUM),
+        ],
+    ),
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N64,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.LARGE),
+        ],
+    ),
+]
 items.append(item5)
 item6 = Item()
 item6.uuid = "9"
 item6.category_uuid = "2"
-item6.small_picture_uuid = "19"
-item6.pictures = ["20","21"]
+item6.small_image = URL + "19.jpg"
+item6.images = [URL + "20.jpg",URL + "21.jpg"]
 item6.title = "Jacket 3"
 item6.description = "Jacket 3 Description"
 item6.price = 400
 item6.point = 5
-item6.options = [Option.custom_init(60,"Medium"),Option.custom_init(54,"Small"),Option.custom_init(64,"Large")]
+# item6.options = [Option.custom_init(60,"Medium"),Option.custom_init(54,"Small"),Option.custom_init(64,"Large")]
+item6.option_types = [OptionTypes.BODY_NUMBER,OptionTypes.SIZE]
+item6.options = [
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N54,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.SMALL),
+        ],
+    ),
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N60,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.MEDIUM),
+        ],
+    ),
+    BodyNumberOption.custom_init(
+        new_value=Numbers.N64,
+        new_options=[
+            SizeOption().custom_init(new_value=Sizes.LARGE),
+        ],
+    ),
+]
 items.append(item6)
 item7 = Item()
 item7.uuid = "10"
 item7.category_uuid = "3"
-item7.small_picture_uuid = "22"
-item7.pictures = ["23","24"]
+item7.small_image = URL + "22.jpg"
+item7.images = [URL + "23.jpg",URL + "24.jpg"]
 item7.title = "Watch 1"
 item7.description = "Watch 1 Description"
 item7.price = 120
 item7.point = 3
-item7.options = [Option.custom_init(10,"Gold"),Option.custom_init(12,"Silver"),Option.custom_init(11,"Platinium")]
+# item7.options = [Option.custom_init(10,"Gold"),Option.custom_init(12,"Silver"),Option.custom_init(11,"Platinium")]
+item7.option_types = [OptionTypes.WRIST_NUMBER,OptionTypes.MATERIAL]
+item7.options = [
+    WristNumberOption.custom_init(
+        new_value=Numbers.N10,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.GOLD),
+        ],
+    ),
+    WristNumberOption.custom_init(
+        new_value=Numbers.N11,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.PLATINIUM),
+        ],
+    ),
+    WristNumberOption.custom_init(
+        new_value=Numbers.N12,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.SILVER),
+        ],
+    ),
+]
 items.append(item7)
 item8 = Item()
 item8.uuid = "11"
 item8.category_uuid = "3"
-item8.small_picture_uuid = "25"
-item8.pictures = ["26","27"]
+item8.small_image = URL + "25.jpg"
+item8.images = [URL + "26.jpg",URL + "27.jpg"]
 item8.title = "Watch 2"
 item8.description = "Watch 2 Description"
 item8.price = 160
 item8.point = 4
-item8.options = [Option.custom_init(10,"Gold"),Option.custom_init(12,"Silver"),Option.custom_init(11,"Platinium")]
+# item8.options = [Option.custom_init(10,"Gold"),Option.custom_init(12,"Silver"),Option.custom_init(11,"Platinium")]
+item8.option_types = [OptionTypes.WRIST_NUMBER,OptionTypes.MATERIAL]
+item8.options = [
+    WristNumberOption.custom_init(
+        new_value=Numbers.N10,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.GOLD),
+        ],
+    ),
+    WristNumberOption.custom_init(
+        new_value=Numbers.N11,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.PLATINIUM),
+        ],
+    ),
+    WristNumberOption.custom_init(
+        new_value=Numbers.N12,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.SILVER),
+        ],
+    ),
+]
 items.append(item8)
 item9 = Item()
 item9.uuid = "12"
 item9.category_uuid = "3"
-item9.small_picture_uuid = "28"
-item9.pictures = ["29","30"]
+item9.small_image = URL + "28.jpg"
+item9.images = [URL + "29.jpg",URL + "30.jpg"]
 item9.title = "Watch 3"
 item9.description = "Watch 3 Description"
 item9.price = 180
 item9.point = 5
-item9.options = [Option.custom_init(10,"Gold"),Option.custom_init(12,"Silver"),Option.custom_init(11,"Platinium")]
+# item9.options = [Option.custom_init(10,"Gold"),Option.custom_init(12,"Silver"),Option.custom_init(11,"Platinium")]
+item9.option_types = [OptionTypes.WRIST_NUMBER,OptionTypes.MATERIAL]
+item9.options = [
+    WristNumberOption.custom_init(
+        new_value=Numbers.N10,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.GOLD),
+        ],
+    ),
+    WristNumberOption.custom_init(
+        new_value=Numbers.N11,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.PLATINIUM),
+        ],
+    ),
+    WristNumberOption.custom_init(
+        new_value=Numbers.N12,
+        new_options=[
+            MaterialOption().custom_init(new_value=Materials.SILVER),
+        ],
+    ),
+]
 items.append(item9)
 
 class Favorite(BaseModel):
